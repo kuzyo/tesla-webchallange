79decf5d8c9042431e56d51acc17adb0417cd411
Yaroslav Kuzyo <yaroslav@jetruby.com>
2016-11-28 12:07:00 +0200

    Add sprites and sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@
 **/
 var gulp         = require('gulp');
 var sass         = require('gulp-sass');
+var sourcemaps   = require('gulp-sourcemaps');
 var browserSync  = require('browser-sync');
 var prefix       = require('gulp-autoprefixer');
 var plumber      = require('gulp-plumber');
@@ -17,6 +18,7 @@ var uglify       = require('gulp-uglify');
 var rename       = require("gulp-rename");
 var imagemin     = require("gulp-imagemin");
 var pngquant     = require('imagemin-pngquant');
+var spritesmith  = require('gulp.spritesmith');
 
 /**
 *
@@ -29,9 +31,11 @@ var pngquant     = require('imagemin-pngquant');
 **/
 gulp.task('sass', function() {
   gulp.src('sass/**/*.scss')
+  .pipe(sourcemaps.init())
   .pipe(sass({outputStyle: 'compressed'}))
   .pipe(prefix('last 2 versions', '> 1%', 'ie 8', 'Android 2', 'Firefox ESR'))
   .pipe(plumber())
+  .pipe(sourcemaps.write('maps')) //path relative to the gulp.dest()s
   .pipe(gulp.dest('css'));
 });
 
@@ -73,6 +77,20 @@ gulp.task('scripts', function() {
 * - Compress them!
 *
 **/
+
+ gulp.task('sprite', function() {
+    var spriteData =
+        gulp.src('images/icons/*')
+            .pipe(spritesmith({
+                imgName: '../images/sprite.png',
+                cssName: '_sprites.scss',
+                cssFormat: 'scss',
+                padding: 2
+            }));
+    spriteData.img.pipe(gulp.dest('./images/'));
+    spriteData.css.pipe(gulp.dest('sass/global'));
+});
+
 gulp.task('images', function () {
   return gulp.src('images/*')
   .pipe(imagemin({
@@ -83,7 +101,6 @@ gulp.task('images', function () {
   .pipe(gulp.dest('images'));
 });
 
-
 /**
 *
 * Default task
@@ -91,7 +108,7 @@ gulp.task('images', function () {
 * - Watchs for file changes for images, scripts and sass/css
 *
 **/
-gulp.task('default', ['sass', 'browser-sync', 'scripts', 'images'], function () {
+gulp.task('default', ['sass', 'browser-sync', 'scripts', 'sprite', 'images'], function () {
   gulp.watch('sass/**/*.scss', ['sass']);
   gulp.watch('js/**/*.js', ['scripts']);
   gulp.watch('images/*', ['images']);
